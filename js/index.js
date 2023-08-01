c93869c43d53edc38e6ae9c14ad88c1d7a44acbc
Jullss <jjuliannn4@gmail.com>
2023-08-01 18:35:44 -0300

    segunda subida

--- js/index.js
+++ js/index.js
@@ -9,11 +9,16 @@ console.log(correoMail)
 
 alert("Hemos registrado su correo con exito! recibira nuestras actualizaciones por este medio: " + correoMail)
 
-// Hasta aca es el inicio, va de 10
+// En este caso se utiliza la divisa ARS como principal tasa de intercambio, por lo tanto USD -> EUR, entre otros, no funcionan.
 
 const conversiones = {
     ARS: 1,
-    USD: 500
+    USD: 500,
+    JPY: 1.93,
+    EUR: 300,
+    CLP: 0.33,
+    GBP: 350,
+    BRL: 57
 };
   
 function convertidor(cantidad, entrada, salida) {
@@ -28,14 +33,14 @@ function convertidor(cantidad, entrada, salida) {
         return;
     }
 
-    const montoConvertido = cantidad * (conversiones[entrada] / conversiones[salida])
+    const montoConvertido = parseFloat((cantidad * (conversiones[entrada] / conversiones[salida])).toFixed(2));
     return cantidad + " " + entrada + " = " + montoConvertido + " " + salida
 }
-  
-for (let i = 0; i < 2; i++) {
+
+for (let i = 0; i < 1; i++) {
     const cantidad = parseFloat(prompt("Ingresa la cantidad a convertir:"))
-    const entrada = prompt("Ingresa la divisa de origen (ARS o USD):")
-    const salida = prompt("Ingresa la divisa de destino (ARS o USD):")
+    const entrada = prompt("Ingresa la divisa de origen (ARS):")
+    const salida = prompt("Ingresa la divisa de destino (USD, JPY, EUR, CLP, GBP O BRL):")
 
     const resultado = convertidor(cantidad, entrada, salida)
     alert("El resultado de tu conversión: " + resultado)
@@ -43,3 +48,17 @@ for (let i = 0; i < 2; i++) {
 }
 
 let gracias = alert("¡Gracias por elegirnos!")
+
+function actualizacionAutomatica (){
+    const fechaHora = new Date()
+    const fechaHoraAct = fechaHora.toLocaleString()
+    alert("Ultima actualización de divisas: " + fechaHoraAct)
+}
+
+actualizacionAutomatica()
+
+const cuentaDeAct = 10 * 60 * 1000
+
+setInterval(actualizacionAutomatica, cuentaDeAct)
+
+  
